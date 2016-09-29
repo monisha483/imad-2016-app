e9ecc71cc6854aa7f54f11c6d58f2633507be2f1
monisha483 <monisha13251@gmail.com>
2016-09-29 22:55:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,45 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
+
+var articleone={
+    title:'Article one|monisha',
+    heading:'article one',
+    date:'sep 29 2016',
+    content:`
+    <p>This is a content. hey there!hello. This is a content. hey there!hello. This is a content. hey there!hello. This is a content. hey there!hello</p>
+            <p>This is a content. hey there!hello. This is a content. hey there!hello. This is a content. hey there!hello. This is a content. hey there!hello</p>
+            <p>This is a content. hey there!hello. This is a content. hey there!hello. This is a content. hey there!hello. This is a content. hey there!hello</p>
+    `
+};
+function createTemplate(data)
+{
+var htmlTemplate=`
+<html>
+    <head>
+        <title>${title}</title>
+     <link href="/ui/style.css" rel="stylesheet" />
+    
+     </head>
+    <body>
+      <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <div>
+            ${date}
+        </div>
+        <div>
+           ${content}
+        </div>
+       </div>
+    </body>
+    
+    
+</html>`;
+    return htmlTemplate;
+}
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
@@ -11,7 +50,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/article-one', function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleone));
 })
 
 app.get('/article-two', function(req,res){
